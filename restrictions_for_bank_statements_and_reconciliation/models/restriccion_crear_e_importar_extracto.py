0528c5ca4feba8389a66245be1675299b56873a7
Lisdrelys <lisdrelys@gmail.com>
2022-10-04 16:30:19 -0400

    Creacion del archivo xml para crear el grupo que puede crear e importar extractos bancarios

--- restrictions_for_bank_statements_and_reconciliation/models/restriccion_crear_e_importar_extracto.py
+++ restrictions_for_bank_statements_and_reconciliation/models/restriccion_crear_e_importar_extracto.py
@@ -6,17 +6,6 @@ from odoo import models, fields, api, exceptions
 
 class AccountBankStatementInherit(models.Model):
     _inherit = 'account.bank.statement'
-#     _description = 'name.name'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#    @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
 
     @api.model
     def create(self, vals):   
