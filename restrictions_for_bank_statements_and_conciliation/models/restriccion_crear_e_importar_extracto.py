af8b7878d1d906cc0db1839b613ca021f36eae1a
Lisdrelys <lisdrelys@gmail.com>
2022-10-07 09:05:07 -0400

    Creacion del archivo xml para crear el grupo que puede crear e importar extractos bancarios

--- restrictions_for_bank_statements_and_conciliation/models/restriccion_crear_e_importar_extracto.py
+++ restrictions_for_bank_statements_and_conciliation/models/restriccion_crear_e_importar_extracto.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-
 from odoo import models, fields, api, exceptions
-
 
 class AccountBankStatementInherit(models.Model):
     _inherit = 'account.bank.statement'
@@ -12,11 +10,10 @@ class AccountBankStatementInherit(models.Model):
         
         res = super(AccountBankStatementInherit,self).create(vals)
         u = self.env['res.users'].search([('id', '=', self.env.uid)])
-        if(not u.has_group('restrictions_for_bank_statements_and_conciliation.group_crear_e_importar_extractos_bancarios')):
+        if not u.has_group('restrictions_for_bank_statements_and_conciliation.group_crear_e_importar_extractos_bancarios'):
            raise exceptions.UserError('No tienes permiso para crear extractos bancarios.')    
 
         return res
-
 
 class AccountMoveLineInherit(models.Model):
     _inherit = "account.move.line"
@@ -25,11 +22,10 @@ class AccountMoveLineInherit(models.Model):
     def action_reconcile(self):
         res = super(AccountMoveLineInherit,self).action_reconcile()
         u = self.env['res.users'].search([('id', '=', self.env.uid)])
-        if(not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios')):
+        if not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios'):
            raise exceptions.UserError('No tienes permiso para conciliar extractos bancarios.')    
 
         return res
-
 
 class AccountAccountInherit(models.Model):
     _inherit = "account.account"
@@ -38,7 +34,7 @@ class AccountAccountInherit(models.Model):
     def action_open_reconcile(self):
         res = super(AccountAccountInherit,self).action_open_reconcile()
         u = self.env['res.users'].search([('id', '=', self.env.uid)])
-        if(not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios')):
+        if not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios'):
            raise exceptions.UserError('No tienes permiso para conciliar extractos bancarios.')        
         return res
 
@@ -49,7 +45,7 @@ class AccountReportInherit(models.AbstractModel):
     def action_partner_reconcile(self, options, params):
         res = super(AccountReportInherit,self).action_partner_reconcile(options, params)
         u = self.env['res.users'].search([('id', '=', self.env.uid)])
-        if(not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios')):
+        if not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios'):
            raise exceptions.UserError('No tienes permiso para conciliar extractos bancarios.')        
         return res
 
@@ -60,6 +56,6 @@ class AccountReconciliationInherit(models.AbstractModel):
     def get_move_lines_for_manual_reconciliation(self, account_id, partner_id=False, excluded_ids=None, search_str=False, offset=0, limit=None, target_currency_id=False):
         res = super(AccountReconciliationInherit,self).get_move_lines_for_manual_reconciliation( account_id, partner_id=False, excluded_ids=None, search_str=False, offset=0, limit=None, target_currency_id=False)
         u = self.env['res.users'].search([('id', '=', self.env.uid)])
-        if(not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios')):
+        if not u.has_group('restrictions_for_bank_statements_and_conciliation.group_conciliar_extractos_bancarios'):
            raise exceptions.UserError('No tienes permiso para conciliar extractos bancarios.')        
         return res
