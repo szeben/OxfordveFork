b5a6156e2c771aa01ba700103c9534b1f74738ce
szeben <juliusjaeger@gmail.com>
2022-10-10 16:38:03 -0400

    restriction bank statement

---
+++ restrictions_for_bank_statements_and_conciliation/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Restricciones para extractos bancarios y conciliacion",
+
+    'summary': """
+        Restringe el proceso de gestion de extractos bancarios y conciliacion de pagos""",
+
+    'description': """
+        Crea grupos de usuarios especiales para la gestion de extractos bancarios (guardar, publicar y validar) y la conciliacion de pagos.
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    'website': "https://technestudioit.com/",
+
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting/Restricciones para extractos bancarios y conciliacion ',
+    'version': '1.0',
+
+    'license': 'Other proprietary',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'account', 'branch'],
+
+    # always loaded
+    'data': [
+        'security/crear_e_importar_extractos_bancarios.xml',
+        'views/account_bank_statement_restrictions.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
