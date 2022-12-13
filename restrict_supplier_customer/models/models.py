2f743105f11b1c3b296e3d122354e540fbb8e8db
Yenbust <yenbust@gmail.com>
2022-12-13 16:26:42 -0400

    Eliminé el módulo.

--- restrict_supplier_customer/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class ContactState(models.Model):
-    _inherit = 'res.partner' 
-    
-    is_national =  fields.Boolean(string="National")
