be1cd3f28bde13e075503ab243c30b6e232caa67
Yenbust <yenbust@gmail.com>
2022-12-19 17:32:39 -0400

    Menus nuevos Informes y contabilidad Oxford.

--- show_account_menu/__manifest__.py
+++ show_account_menu/__manifest__.py
@@ -6,15 +6,19 @@
         Permite la visualización de algunas opciones del menú y sub-menú, en el módulo de contabilidad.""",
 
     'description': """
-        Permite la visualización de algunas opciones del menú y submenú, en el módulo de contabilidad.       
-        El módulo crea 7 grupos de usuarios, que permite cada uno respectivamente:
+        El módulo agrega dos opciones de menú:
+        1) Contabilidad Oxford.
+        2) Informes Oxford.
+      
+        El módulo crea 8 grupos de usuarios, que permite cada uno respectivamente:
         1) Visualizar el menú "Clientes.
         2) Visualizar el menú "Proveedores".
         3) Visualizar el menú "Contabilidad".
-        4) Visualizar el menú "Clientes->Pagos".
+        4) visualizar el menú "Informes ".       
         5) Visualizar el sub menú "Pagos" del menú "Clientes".
         6) Visualizar el sub menú "Libro Mayor" del menú "Contabilidad" e "Informe".
         7) Visualizar el sub menú "Vencidos por cobrar" del menú "Informes".
+        8) Visualizar el sub menú "Vencidos por pagar" del menú "Informes".       
     """,
 
     'author': "Techne Studio IT & Consulting",
@@ -27,11 +31,12 @@
 
     'license': "Other proprietary",
 
-    'depends': ['base', 'sale', 'purchase', 'account'],
+    'depends': ['base', 'sale', 'purchase', 'account', 	'account_accountant'],
 
     "installable": True,
     
-    "data": [
+    "data": [     
             'security/show_account_menu.xml',
+            'security/ir.model.access.csv',
             ],
 }
