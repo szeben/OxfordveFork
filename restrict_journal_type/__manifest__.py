36c60c27563d5208ac5547c0a203209c9e788ae8
szeben <juliusjaeger@gmail.com>
2022-12-27 15:47:41 -0400

    restrict journal type

---
+++ restrict_journal_type/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Restricciones por tipo de diario.",
+
+    'summary': """
+        Restringe la visualización de los diarios de acuerdo a su clasificación 
+        como custodio ó inversión.""",
+
+    'description': """
+       Incluye una clasificación para los diarios, identificándolos como 
+       “Custodio” ó “Inversión”, a fin de que sólo ciertos usuarios puedan 
+       visualizar dichos diarios.
+
+       Agrega 2 grupo de usuario:
+       1) Grupo para visualizar diarios de tipo Cutodio.
+       2) Grupo para visualizar diarios de tipo Inversión.
+
+    """,
+
+    'author': "Techne Studio IT & Consulting",
+    
+    'website': "https://technestudioit.com/",
+  
+    'category': 'Accounting/Journal Type.',
+
+    'version': '1.0',
+
+    'license': "Other proprietary",
+
+    'depends': ['base', 'account'],
+
+    "installable": True,
+    
+    "data": [            
+            'security/restrict_journal_type.xml',
+            'security/ir.model.access.csv',
+            'views/journal_type.xml',       
+            ],
+}
