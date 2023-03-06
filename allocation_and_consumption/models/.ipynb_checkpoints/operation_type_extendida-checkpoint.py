c3d5d7a1ebd8f3db27ab41c9ade03d3ea9dd970b
szeben <juliusjaeger@gmail.com>
2023-03-06 07:59:00 -0400

    install allocation_and_consumption

---
+++ allocation_and_consumption/models/.ipynb_checkpoints/operation_type_extendida-checkpoint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class OperationTypeExtendida(models.Model):
+    _inherit = "stock.picking.type"
+
+    asignacion_y_consumo_interno = fields.Boolean(string="¿Es para asignación y consumo interno?")
+    
+class OperationExtendida(models.Model):
+    _inherit = "stock.picking"
+
+    picking_seq_code = fields.Char(related='picking_type_id.sequence_code', readonly=True)
+    
+
+
+
