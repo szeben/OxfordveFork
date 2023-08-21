78bc1b9db3d0227ba1a4bd7b25793488781d3c5d
szeben <juliusjaeger@gmail.com>
2023-08-21 19:21:36 -0400

    Update tsc_control_cash

--- tsc_control_cash_payments/models/models.py
+++ tsc_control_cash_payments/models/models.py
@@ -38,3 +38,36 @@ class TSCAccountPayment(models.Model):
             self.tsc_check_user_group = True
         else:
             self.tsc_check_user_group = False
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        # OVERRIDE
+        for vals in vals_list:
+            search_journal = self.env['account.journal'].search([('id', '=', vals['journal_id'])])
+            if search_journal and search_journal.type == 'cash':
+                res_user = self.env['res.users'].search([('id', '=', self._uid)])
+
+                if res_user.has_group('tsc_control_cash_payments.tsc_make_cash_payment_group'):
+                    continue
+                else:
+                    raise UserError(_("You are not authorized to record cash payments."))
+        return super(TSCAccountPayment, self).create(vals_list)
+
+class TSCAccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    tsc_check_user_group = fields.Boolean(
+        string="Revisa el grupo del usuario activo",
+        compute="_compute_tsc_check_user_group",
+        store=False
+    )
+
+    @api.model
+    def _compute_tsc_check_user_group(self):
+        res_user = self.env['res.users'].search([('id', '=', self._uid)])
+
+        for jounal in self:
+            if res_user.has_group('tsc_control_cash_payments.tsc_make_cash_payment_group'):
+                jounal.tsc_check_user_group = True
+            else:
+                jounal.tsc_check_user_group = False
