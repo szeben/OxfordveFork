6f44a7d0a0e7c72a5405b25e61a50db60a371fa9
julieta <thethyhun@hotmail.com>
2023-10-26 11:01:55 +0000

    todo menos traducciones

--- tsc_restriction_on_unlocking_and_cancellation/models/tsc_account_move_model.py
+++ tsc_restriction_on_unlocking_and_cancellation/models/tsc_account_move_model.py
@@ -1,59 +1,46 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models, tools
+from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError
 from lxml import etree
-
+import simplejson
 
 class tsc_AccountMove(models.Model):
 
     _inherit = 'account.move'
 
-    tsc_posted_once = fields.Boolean(string="Was it posted once?", default=False)
-
     def button_cancel(self):
-        """
-        tsc_sale_order = self.invoice_origin
-        self.tsc_posted_once = True
-        if self.move_type == "out_invoice" and tsc_sale_order:
-            tsc_picking_id = tsc_sale_order.picking_ids
-            if tsc_picking_id.picking_type_code == "outgoing" and tsc_picking_id.state == "done":
-                raise UserError(_("It is not possible to cancel the invoice that has confirmed merchandise already dispatched. Please try to generate a credit note."))
-        """
+        if self.move_type == "out_invoice":
+            tsc_sale_order = self.invoice_origin
+            if tsc_sale_order:
+                tsc_sale_order_search = self.env['sale.order'].search([
+                    ('name','=',tsc_sale_order),
+                    ('picking_ids.picking_type_code','=','outgoing'),
+                    ('picking_ids.state','=','done')])
+                if tsc_sale_order_search.exists():
+                    raise UserError(_("It is not possible to cancel the invoice that has confirmed merchandise already dispatched. Please try to generate a credit note."))
         self.write({'auto_post': False, 'state': 'cancel'})
 
     
-    def read(self, fields, load='_classic_read'):
-        if self.check_access_rights('read', raise_exception=False):
-            return super().read(fields, load=load)
-
     #@api.model
     def fields_view_get(self, view_id=None, view_type='form',
                         toolbar=False, submenu=False):
 
-    
         res = super().fields_view_get(
             view_id=view_id, view_type=view_type,
             toolbar=toolbar, submenu=submenu)
-
-        #raise UserError(f'{res}')
-
-
-    
-        doc = etree.XML(res['arch'])
-    
+        doc = etree.XML(res['arch'])    
+        
         if view_type == 'form':
-            for node_form in doc.xpath("//form"):
-                node = doc.xpath("//field[@name='tsc_posted_once']")[0]
-                #raise UserError(f'{node.get("context")}')
-                node_form.set("edit", 'false')
+            for node in doc.xpath("//field"):
+                modifiers = simplejson.loads(node.get("modifiers"))
+                if 'readonly' not in modifiers:
+                    modifiers['readonly'] = ['&amp;', ['posted_before','=',True], ['move_type','in',["out_invoice", "out_refund"]]]
+                elif type(modifiers['readonly']) != bool:
+                        modifiers['readonly'].insert(0, '|')
+                        modifiers['readonly'] += ['&amp;', ['posted_before','=',True], ['move_type','in',["out_invoice", "out_refund"]]]
+                node.set('modifiers', simplejson.dumps(modifiers)) 
                 
 
         res['arch'] = etree.tostring(doc)
         return res
-
-
-
-
-    def write(self,vals):
-        #raise UserError(f'{self.tsc_posted_once}')
-        return super().write(vals)
+     
